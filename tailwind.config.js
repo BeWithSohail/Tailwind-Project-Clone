6d7aee6382a65882d885044ec7bd115447c096bd
BeWithSohail <sohailalam6027@gmail.com>
2023-10-20 02:20:46 +0530

    working on config customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
     content: ["*"],
     theme: {
-        extend: {},
+        screens: {
+            xsm:'375',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+          },
+        extend: {
+            spacing: {
+                18: '4.5rem',
+                22: '6.5rem',
+            },
+        },
     },
     plugins: [],
 }
